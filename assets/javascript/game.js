01e0c40c81e4a74d273f97e47711542d87992039
Dloft <derek.lofton1@gmail.com>
2018-12-10 11:31:59 -0500

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,29 @@
 const computerChoice = ['a', 'b', 'c'];
-const game = document.querySelector('index');
-let Wins = 0,
-    losses = 0,
-    guessesLeft = 7,
-    soFar = (userGuess);
+const game = document.querySelector('main');
+let wins = 0,
+    losses = 0;
+
 
 document.onkeyup = function(event) {
     const userGuess = event.key;
 
-    const computeGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    const computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
 if(userGuess === 'a' || userGuess == 'b' || userGuess === 'c') {
     const userWin =
-    (userGuess === 'a' && computerGuess === 'a')
-    (userGuess === 'b' && computerGuess === 'b')
+    (userGuess === 'a' && computerGuess === 'a') ||
+    (userGuess === 'b' && computerGuess === 'b') ||
     (userGuess === 'c' && computerGuess === 'c');
     if (userWin) {
         wins++;
-    // } else {
-    //     soFar++;
-    // }
+    } else {
+        losses++;
+    }
 
     game.innerHTML = `
-    <p>Wins: ${userGuess}<p>
-    <p>Losses: ${losses}<p>
-    <P>Guesses Left: ${}<p>
-    <p>Your Guesses so far: ${}<p>
+    <p>wins: ${wins}<p>
+    <p>losses: ${losses}<p>
     `
+    }
 }
-}
+
